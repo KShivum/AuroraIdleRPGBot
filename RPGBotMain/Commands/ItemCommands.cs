dfbdeba1e917ce3bb07f39a8a807ea70b2f8d057
Shivum <shivumkumar1@gmail.com>
2021-11-23 01:31:49 -0600

    Added Inventory

--- RPGBotMain/Commands/ItemCommands.cs
+++ RPGBotMain/Commands/ItemCommands.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
 using RPGBotMain.Models;
 
 namespace RPGBotMain.Commands
@@ -14,9 +19,112 @@ namespace RPGBotMain.Commands
         [Command("item")]
         public async Task CreateItem(CommandContext ctx)
         {
-            Item item = new Item("Wooden Sword", 1, 0, null, null, false, ctx.User.Id.ToString());
+            Item item = new Item("Wooden Sword", (int)(new Random()).NextInt64(15), 0, null, null, false, ctx.User.Id.ToString());
             item.Create(con);
             await ctx.RespondAsync($"{ctx.User.Mention} created an item!");
         }
+
+
+
+        [Command("inventory")]
+        public async Task Inventory(CommandContext ctx)
+        {
+            int amountToShow = 15;
+            var interactivity = ctx.Client.GetInteractivity();
+            SqlDataAdapter adapter = new SqlDataAdapter($"Select * from Item WHERE Owner = '{ctx.User.Id}'", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            if (dt.Rows.Count == 0)
+            {
+                var messages = await ctx.RespondAsync("You don't have any items!");
+                await Task.Delay(5000);
+                await ctx.Channel.DeleteMessagesAsync(new List<DiscordMessage> { messages, ctx.Message });
+                return;
+            }
+            try
+            {
+                ctx.Client.ComponentInteractionCreated += async (s, e) =>
+                {
+                    await e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            int startingItem = 0;
+
+            while (true)
+            {
+                var inventoryMessage = new DiscordMessageBuilder();
+                var embed = new DiscordEmbedBuilder();
+                embed.WithTitle("Inventory");
+                embed.WithColor(DiscordColor.Blue);
+
+                for (int i = startingItem; i < startingItem + amountToShow; i++)
+                {
+                    if (i >= dt.Rows.Count)
+                    {
+                        break;
+                    }
+
+                    embed.AddField($"Id: {dt.Rows[i]["Id"]}", $"{dt.Rows[i]["ItemName"]}: {dt.Rows[i]["Stat1"]} {dt.Rows[i]["Stat2"]} {dt.Rows[i]["Stat3"]} {dt.Rows[i]["Stat4"]}");
+                }
+
+                var LeftArrow = new DiscordButtonComponent(DSharpPlus.ButtonStyle.Primary, "Left", "Left", false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":arrow_left:")));
+                var Exit = new DiscordButtonComponent(DSharpPlus.ButtonStyle.Primary, "Exit", "Exit", false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":x:")));
+                var RightArrow = new DiscordButtonComponent(DSharpPlus.ButtonStyle.Primary, "Right", "Right", false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":arrow_right:")));
+
+                if (startingItem == 0)
+                {
+                    LeftArrow.Disable();
+                }
+
+                if (startingItem + amountToShow >= dt.Rows.Count)
+                {
+                    RightArrow.Disable();
+                }
+
+
+
+                inventoryMessage.WithEmbed(embed);
+
+                inventoryMessage.AddComponents(LeftArrow, Exit, RightArrow);
+
+                var sentMessage = await ctx.Channel.SendMessageAsync(inventoryMessage);
+
+
+                var buttonResult = await interactivity.WaitForButtonAsync(sentMessage, new List<DiscordButtonComponent> { LeftArrow, RightArrow, Exit }, TimeSpan.FromMinutes(2));
+
+                if (buttonResult.TimedOut)
+                {
+                    await ctx.Channel.DeleteMessagesAsync(new List<DiscordMessage> { sentMessage, ctx.Message });
+                    return;
+                }
+
+                if (buttonResult.Result.Id == "Left")
+                {
+                    startingItem -= amountToShow;
+                }
+                else if (buttonResult.Result.Id == "Right")
+                {
+                    startingItem += amountToShow;
+                }
+                else if (buttonResult.Result.Id == "Exit")
+                {
+                    await ctx.Channel.DeleteMessagesAsync(new List<DiscordMessage> { sentMessage, ctx.Message });
+                    return;
+                }
+
+                await ctx.Channel.DeleteMessagesAsync(new List<DiscordMessage> { sentMessage });
+
+
+            }
+
+        }
+
+
     }
 }
--- RPGBotMain/Models/Item.cs
+++ RPGBotMain/Models/Item.cs
@@ -15,6 +15,7 @@ namespace RPGBotMain.Models
         public bool IsEquipped { get; set; }
         public string Owner { get; set; }
 
+
         public Item(string name, int? stat1, int? stat2, int? stat3, int? stat4, bool isEquipped, string owner)
         {
             Name = name;
@@ -24,6 +25,9 @@ namespace RPGBotMain.Models
             Stat4 = stat4;
             IsEquipped = isEquipped;
             Owner = owner;
+        }
+        public Item()
+        {
         }
 
         public Item(SqlConnection con, int id)
@@ -48,6 +52,8 @@ namespace RPGBotMain.Models
             Owner = dt.Rows[0]["Owner"].ToString();
         }
 
+
+
         public void Create(SqlConnection con)
         {
             string query = $"INSERT INTO Item (ItemName, Stat1, Stat2, Stat3, Stat4, Equipped, Owner) VALUES ('{Name}',";
@@ -65,14 +71,14 @@ namespace RPGBotMain.Models
                 query += $"{Stat3},";
             else
                 query += "NULL,";
-            
+
             if (Stat4 != null)
                 query += $"{Stat4},";
             else
                 query += "NULL,";
 
 
-            if(IsEquipped)
+            if (IsEquipped)
                 query += "1,";
             else
                 query += "0,";
@@ -81,5 +87,31 @@ namespace RPGBotMain.Models
             SqlCommand cmd = new SqlCommand(query, con);
             cmd.ExecuteNonQuery();
         }
+
+        public void GetItemFromID(int Id, SqlConnection con)
+        {
+
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM Item WHERE Id = {Id}", con);
+            adapter.Fill(dt);
+
+            this.Id = Id;
+            Name = dt.Rows[0]["Name"].ToString();
+            if (dt.Rows[0]["Stat1"] != DBNull.Value)
+                Stat1 = Convert.ToInt32(dt.Rows[0]["Stat1"]);
+            if (dt.Rows[0]["Stat2"] != DBNull.Value)
+                Stat2 = Convert.ToInt32(dt.Rows[0]["Stat2"]);
+            if (dt.Rows[0]["Stat3"] != DBNull.Value)
+                Stat3 = Convert.ToInt32(dt.Rows[0]["Stat3"]);
+            if (dt.Rows[0]["Stat4"] != DBNull.Value)
+                Stat4 = Convert.ToInt32(dt.Rows[0]["Stat4"]);
+
+            IsEquipped = Convert.ToBoolean(dt.Rows[0]["IsEquipped"]);
+            Owner = dt.Rows[0]["Owner"].ToString();
+
+        }
     }
+
+
 }
